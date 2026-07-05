030a9d9f301e84e765a27b5ba00718bf9d615382
Ritwik-coder <ritwik@websoftline.com>
2026-07-05 13:49:04 -0400

    Add files via upload

---
+++ Alphabets.py
@@ -0,0 +1,56 @@
+character = str(input("Give me a charecter"))
+
+if character == "a" or character =="A" :
+  print("Your Charecter is a alpahabet")
+elif character == "b" or character =="B" :
+  print("Your Charecter is a alpahabet")
+elif character == "c" or character =="C" :
+  print("Your Charecter is a alpahabet")
+elif character == "d" or character =="D" :
+  print("Your Charecter is a alpahabet")
+elif character == "e" or character =="E" :
+  print("Your Charecter is a alpahabet")
+elif character == "f" or character =="F" :
+  print("Your Charecter is a alpahabet")
+elif character == "g" or character =="G" :
+  print("Your Charecter is a alpahabet")
+elif character == "h" or character =="H" :
+  print("Your Charecter is a alpahabet")
+elif character == "i" or character =="I" :
+  print("Your Charecter is a alpahabet")
+elif character == "j" or character =="J" :
+  print("Your Charecter is a alpahabet")
+elif character == "k" or character =="K" :
+  print("Your Charecter is a alpahabet")
+elif character == "l" or character =="L" :
+  print("Your Charecter is a alpahabet")
+elif character == "m" or character =="M" :
+  print("Your Charecter is a alpahabet")
+elif character == "n" or character =="N" :
+  print("Your Charecter is a alpahabet")
+elif character == "o" or character =="O" :
+  print("Your Charecter is a alpahabet")
+elif character == "p" or character =="P" :
+  print("Your Charecter is a alpahabet")
+elif character == "q" or character =="Q" :
+  print("Your Charecter is a alpahabet")
+elif character == "r" or character =="R" :
+  print("Your Charecter is a alpahabet")
+elif character == "s" or character =="S" :
+  print("Your Charecter is a alpahabet")
+elif character == "t" or character =="T" :
+  print("Your Charecter is a alpahabet")
+elif character == "u" or character =="U" :
+  print("Your Charecter is a alpahabet")
+elif character == "v" or character =="V" :
+  print("Your Charecter is a alpahabet")
+elif character == "w" or character =="W" :
+  print("Your Charecter is a alpahabet")
+elif character == "x" or character =="X" :
+  print("Your Charecter is a alpahabet")
+elif character == "y" or character =="Y" :
+  print("Your Charecter is a alpahabet")
+elif character == "z" or character =="Z" :
+  print("Your Charecter is a alpahabet")
+else:
+  print("Your charecter is not an alphabet") 
